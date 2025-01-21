8645f1b5df353f1d3d8b250bfebd0739835de882
akhlaquerahman <akhlaquerahman18@gmail.com>
2025-01-21 10:19:20 +0530

    print first program of js

--- Java Script/DOMfirst.js
+++ Java Script/DOMfirst.js
@@ -1,3 +1,44 @@
-let a = "Akhlaque";
+// let a = "Akhlaque";
 
-console.log(a);
+// console.log(a);
+
+
+/* document.all[7].innerText="Rahman"
+
+ let a = document.getElementById("myid");
+
+ document.all[7].innerText="R"  */
+
+
+// let a=document.querySelector("h1").innerText="bnnn"
+// console.log(a)
+
+
+// let paraa=document.querySelector("para")
+
+
+/*
+let b=document.querySelector("h1");
+b.style.color="blue"
+
+*/
+
+/*
+let a=document.querySelectorAll(".list li");
+
+for(let i=0; i<a.length; i++){
+    a[i].style.color="red";
+    console.log(a[i]);
+}
+
+*/
+
+/*
+let a=document.querySelector("h1")
+
+a.classList.toggle("green")     // means create clsss
+a.classList.toggle("green")     // means delete class
+*/
+
+let btn=document.querySelector("button")
+btn.innerHTML="Click";
