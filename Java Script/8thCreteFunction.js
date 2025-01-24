22f960cd17cfd84df0386a0075fd8e0e0e50879d
akhlaquerahman <akhlaquerahman18@gmail.com>
2025-01-24 17:22:48 +0530

    Crete Function in java script

---
+++ Java Script/8thCreteFunction.js
@@ -0,0 +1,14 @@
+function a(){
+    console.log("Create Function")
+}
+
+function add(){
+    let a = 10
+    let b = 20
+
+    let c = a+b
+    console.log(`The Sum Of two Number : ${c}`)
+}
+
+a()
+add()
