c4ef21105fffe514573c1663b1489f440ec1b68e
akhlaquerahman <akhlaquerahman18@gmail.com>
2025-01-25 09:52:06 +0530

    Print Positive and negative Number

---
+++ Java Script/11thCheckPositiveorNegative.js
@@ -0,0 +1,14 @@
+function checkNumber(num){
+    if(num>0){
+        console.log(`${num}   is a positive number`);
+    }
+    else if(num<0){
+        console.log(`${num}   is a negative number`);
+        }
+        else{
+            console.log(`${num}   is zero`);
+            }
+}
+checkNumber(0)
+checkNumber(10)
+checkNumber(-15)
