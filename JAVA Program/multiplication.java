63226686a71ac7319affc9dd87f17cbd9d4fe4cc
akhlaquerahman <akhlaquerahman18@gmail.com>
2025-01-30 09:38:15 +0530

    Java Program multiply of two number

---
+++ JAVA Program/multiplication.java
@@ -0,0 +1,11 @@
+public class multiplication {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+
+        int multi = a*b;
+
+        System.out.println("The Multiply of two Number = " + multi);
+    }
+    
+}
