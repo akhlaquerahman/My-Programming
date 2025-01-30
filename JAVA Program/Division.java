b23a4edf187810979454771cb30de99b61558b12
akhlaquerahman <akhlaquerahman18@gmail.com>
2025-01-30 09:39:24 +0530

    Java Program Division of two number

---
+++ JAVA Program/Division.java
@@ -0,0 +1,12 @@
+
+public class Division {
+    public static void main (String[] args){
+        int a = 30;
+        int b = 6;
+
+        int divide = a/b;
+
+        System.out.println("The division of two Number: = "+divide);
+    }
+    
+}
