eba28d5c9d5cbee9ab7bc4bf15c06ab34f1572ef
akhlaquerahman <akhlaquerahman18@gmail.com>
2025-01-30 09:40:21 +0530

    Even Or Odd Number

---
+++ JAVA Program/EvenOrOdd.java
@@ -0,0 +1,15 @@
+
+
+public class EvenOrOdd {
+    public static void main(String[] args){
+        int a = 23;
+
+        if(a%2==0){
+            System.out.println("The number is even: = "+a);
+        }
+        else{
+            System.out.println("The number is odd: = "+a);
+        }
+    }
+    
+}
